08a3e2858196a38808ca371f8763f111c019d072
yunghog <samarthahm@gmail.com>
2020-08-16 19:33:34 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import home from "./pages/home";
+class App extends Component {
+  render() {
+    return (
+        <Router>
+          <Route path="/" exact component={home}/>
+        </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../css/style.css";
+
+class home extends Component {
+  render() {
+    return (
+        <div className="body-section">
+          <div className="container">
+            <h1>Home</h1>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default home;
