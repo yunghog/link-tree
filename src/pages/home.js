5d74e34373d25425d543e3e2011b7addd02c9d26
yunghog <samarthahm@gmail.com>
2020-08-17 13:00:54 +0530

    added animation

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,10 +4,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faYoutube, faInstagram, faFacebook, faWhatsapp, faLinkedin, faSoundcloud } from '@fortawesome/free-brands-svg-icons'
 import {faEnvelope, faGlobe} from  '@fortawesome/free-solid-svg-icons'
 import "../css/style.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import hero1 from "../images/hero-1.jpg";
 import avatar from "../images/avatar.jpg";
 class home extends Component {
   render() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
     function darkTheme(){
       if(document.body.getAttribute('data-theme')=="light"){
         document.body.removeAttribute('data-theme');
@@ -16,6 +22,8 @@ class home extends Component {
         document.body.setAttribute('data-theme', 'light');
       }
     }
+    document.body.setAttribute('data-aos','fade');
+    document.body.setAttribute('data-aos-delay','300');
     return (
       <ga>
       <Container fluid id="themeSwitch">
@@ -27,14 +35,14 @@ class home extends Component {
             <div className="avatar">
               <img src={avatar}/>
             </div>
-            <div className="title-text"><h1>Samartha</h1><h5>Developer | Beatmaker | Graphic Designer</h5></div>
+            <div className="title-text"  data-aos="fade-up"><h1>Samartha</h1><h5>Developer | Beatmaker | Graphic Designer</h5></div>
           </div>
         </Col></Row>
       </Container>
       <br/>
       <Container>
       <Row xs={1} md={3}>
-        <Col>
+        <Col data-aos="fade-right" data-aos-delay={300}>
           <a href="http://github.com/yunghog" target="_blank">
             <div className="link-card">
               <Row noGutters>
@@ -49,7 +57,7 @@ class home extends Component {
             </div>
           </a>
         </Col>
-        <Col>
+        <Col data-aos="fade-right" data-aos-delay={400}>
           <a href="http://youtube.com/c/YUNGHOGbeats" target="_blank">
             <div className="link-card">
               <Row noGutters>
@@ -64,7 +72,7 @@ class home extends Component {
             </div>
           </a>
         </Col>
-        <Col>
+        <Col data-aos="fade-right" data-aos-delay={500}>
           <a href="http://instagram.com/yungh_og_beats" target="_blank">
             <div className="link-card">
               <Row noGutters>
@@ -79,7 +87,7 @@ class home extends Component {
             </div>
           </a>
         </Col>
-        <Col>
+        <Col data-aos="fade-right" data-aos-delay={300}>
           <a href="https://soundcloud.com/yunghog" target="_blank">
             <div className="link-card">
               <Row noGutters>
@@ -94,7 +102,7 @@ class home extends Component {
             </div>
           </a>
         </Col>
-        <Col>
+        <Col data-aos="fade-right" data-aos-delay={400}>
           <a href="https://www.linkedin.com/in/samartha-hm-7398861a1/" target="_blank">
             <div className="link-card">
               <Row noGutters>
@@ -108,7 +116,7 @@ class home extends Component {
             </div>
           </a>
         </Col>
-        <Col>
+        <Col data-aos="fade-right" data-aos-delay={500}>
           <a href="mailto:samarthaog@gmail.com" target="_blank">
             <div className="link-card">
               <Row noGutters>
@@ -123,7 +131,7 @@ class home extends Component {
             </div>
           </a>
         </Col>
-        <Col>
+        <Col data-aos="fade-right" data-aos-delay={300}>
           <a href="http://www.ms800hub.rf.gd" target="_blank">
             <div className="link-card">
               <Row noGutters>
@@ -138,7 +146,7 @@ class home extends Component {
             </div>
           </a>
         </Col>
-        <Col>
+        <Col data-aos="fade-right" data-aos-delay={400}>
           <a href="https://yunghog.github.io" target="_blank">
             <div className="link-card">
               <Row noGutters>
@@ -153,7 +161,7 @@ class home extends Component {
             </div>
           </a>
         </Col>
-        <Col>
+        <Col data-aos="fade-right" data-aos-delay={500}>
           <a href="https://api.whatsapp.com/send?phone=918904460742" target="_blank">
             <div className="link-card">
               <Row noGutters>
