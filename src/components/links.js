c13eaed43118f7ca54034525addc0ca15c991390
yunghog <samarthahm@gmail.com>
2020-09-22 20:04:24 +0530

    created json file that holds alll the link data

---
+++ src/components/links.js
@@ -0,0 +1,66 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faYoutube, faInstagram, faFacebook, faWhatsapp, faLinkedin, faSoundcloud } from '@fortawesome/free-brands-svg-icons'
+import {faEnvelope, faGlobe} from  '@fortawesome/free-solid-svg-icons'
+
+export const Links=[
+  {
+    "site":"Github",
+    "username":"@yunghog",
+    "link":"https://github.com/yunghog",
+    "icon":faGithub
+  },
+  {
+    "site":"YouTube",
+    "username":"YUNGH OG Beats",
+    "link":"https://youtube.com/c/YUNGHOGbeats",
+    "icon":faYoutube
+  },
+  {
+    "site":"Instagram",
+    "username":"@yungh_og_beats",
+    "link":"http://instagram.com/yungh_og_beats",
+    "icon":faInstagram
+  },
+  {
+    "site":"Soundcloud",
+    "username":"YUNGH OG Beats",
+    "link":"https://soundcloud.com/yunghog",
+    "icon":faSoundcloud
+  },
+  {
+    "site":"linnkedin",
+    "username":"@Samartha H M",
+    "link":"https://www.linkedin.com/in/samartha-hm-7398861a1/",
+    "icon":faLinkedin
+  },
+  {
+    "site":"Mail ID",
+    "username":"samarthaog@gmal.com",
+    "link":"mailto:samarthaog@gmail.com",
+    "icon":faEnvelope
+  },
+  {
+    "site":"MS800 Hub",
+    "username":"Join the crew",
+    "link":"http://ms800hub.rf.gd",
+    "icon":faGlobe
+  },
+  {
+    "site":"My portfolio",
+    "username":"Visit",
+    "link":"https://yunghog.github.io",
+    "icon":faGlobe
+  },
+  {
+    "site":"My blog",
+    "username":"Creed Thoughts",
+    "link":"https://creedthoughts.herokuapp.com",
+    "icon":faGlobe
+  },
+  {
+    "site":"Whatsapp",
+    "username":"Text",
+    "link":"https://api.whatsapp.com/send?phone=918904460742",
+    "icon":faWhatsapp
+  }
+];
