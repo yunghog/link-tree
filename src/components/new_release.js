6fab66b8d1b1434c0844253ec7baa221701c67b4
Samartha <samarthahm@gmail.com>
2021-03-07 14:39:45 +0530

    release json

---
+++ src/components/new_release.js
@@ -0,0 +1,16 @@
+export const Releases=[
+  {
+    "id":"ka",
+    "name":"KA - yunghog flip",
+    "yt":"https://github.com/yunghog",
+    "sc":"https://github.com/yunghog",
+    "igtv":"https://github.com/yunghog"
+  },
+  {
+    "id":"neer",
+    "name":"neer - yunghog remix",
+    "yt":"https://github.com/yunghog",
+    "sc":"https://github.com/yunghog",
+    "igtv":"https://github.com/yunghog"
+  }
+];
