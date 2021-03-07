4abd6a4c28ecabc77118875e9b304dd7b674f602
Samartha <samarthahm@gmail.com>
2021-03-07 14:39:52 +0530

    release  page

--- src/pages/release.js
+++ src/pages/release.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 import {Container, Row, Col} from 'react-bootstrap';
+import {Releases} from '../components/new_release.js'
 import "../css/style.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 class Release extends Component {
+  get_release(x){
+    for(var i=0;i<Releases.length;i++){
+      if(Releases[i].id==x){
+        return(Releases[i]);
+      }
+    }
+  }
+  data=this.get_release(this.props.match.params.release);
   render() {
     AOS.init({
       duration: 1000,
@@ -13,7 +22,10 @@ class Release extends Component {
       <div  id="content-wrapper">
       <Container fluid>
       <h1>HAAAAAAAAA</h1>
-      <h1>{this.props.match.params.release}</h1>
+      {this.data.name}
+      {this.data.yt}
+      {this.data.sc}
+      {this.data.igtv}
       </Container>
       </div>
     );
